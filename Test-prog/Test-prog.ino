4f238262f78eeb9d2209a05edf08a5c3cce6a7b3
Joeri Chantrel <74982195+JChantrel@users.noreply.github.com>
2021-02-10 17:34:57 +0100

    Update Test-prog.ino

--- Test-prog/Test-prog.ino
+++ Test-prog/Test-prog.ino
@@ -1,18 +1,21 @@
 #include <Keypad.h>
 
-const byte ROWS = 7;
-const byte COLS = 10;
+const byte ROWS = 10;
+const byte COLS = 7;
 char hexaKeys[ROWS][COLS] = {
-  {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'},
-  {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'},
-  {'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'},
-  {'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd'},
-  {'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'},
-  {'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x'},
-  {'y', 'z', '!', '@', '#', '$', '%', '^', '&', '*'},
+  {'0', '1', '2', '3', '4', '5', '6'},
+  {'A', 'B', 'C', 'D', 'E', 'F', 'G'},
+  {'K', 'L', 'M', 'N', 'O', 'P', 'Q'},
+  {'U', 'V', 'W', 'X', 'Y', 'Z', 'a'},
+  {'e', 'f', 'g', 'h', 'i', 'j', 'k'},
+  {'o', 'p', 'q', 'r', 's', 't', 'u'},
+  {'y', 'z', '!', '@', '#', '$', '%'},
+  {'7', '8', '9', 'H', 'I', 'J', 'R'},
+  {'S', 'T', 'b', 'c', 'd', 'l', 'm'},
+  {'n', 'v', 'w', 'x', '^', '&', '*'},
 };
-byte colPins[COLS] = {36, 39, 34, 35, 32, 33, 25, 26, 27, 14};
-byte rowPins[ROWS] = {19, 18, 5, 17, 16, 8, 7};
+byte rowPins[ROWS] = {13, 12, 14, 27, 26, 25, 33, 32, 35, 34};
+byte colPins[COLS] = {15, 2, 4, 5, 18, 19, 21};
 
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
@@ -22,219 +25,219 @@ void setup() {
 
 void loop() {
   char customKey = customKeypad.getKey();
-  Serial.println(customKeypad.getState());
+  //Serial.print('A');
   
   if (customKey) {
     switch (customKey) {
       case '0':
-        Serial.write('0');
+        Serial.print('0');
         break;
       case '1':
-        Serial.write('1');
+        Serial.print('1');
         break;
       case '2':
-        Serial.write('2');
+        Serial.print('2');
         break;
       case '3':
-        Serial.write('3');
+        Serial.print('3');
         break;
       case '4':
-        Serial.write('4');
+        Serial.print('4');
         break;
       case '5':
-        Serial.write('5');
+        Serial.print('5');
         break;
       case '6':
-        Serial.write('6');
+        Serial.print('6');
         break;
       case '7':
-        Serial.write('7');
+        Serial.print('7');
         break;
       case '8':
-        Serial.write('8');
+        Serial.print('8');
         break;
       case '9':
-        Serial.write('9');
+        Serial.print('9');
         break;
       case 'A':
-        Serial.write('A');
+        Serial.print('A');
         break;
       case 'B':
-        Serial.write('B');
+        Serial.print('B');
         break;
       case 'C':
-        Serial.write('C');
+        Serial.print('C');
         break;
       case 'D':
-        Serial.write('D');
+        Serial.print('D');
         break;
       case 'E':
-        Serial.write('E');
+        Serial.print('E');
         break;
       case 'F':
-        Serial.write('F');
+        Serial.print('F');
         break;
       case 'G':
-        Serial.write('G');
+        Serial.print('G');
         break;
       case 'H':
-        Serial.write('H');
+        Serial.print('H');
         break;
       case 'I':
-        Serial.write('I');
+        Serial.print('I');
         break;
       case 'J':
-        Serial.write('J');
+        Serial.print('J');
         break;
       case 'K':
-        Serial.write('K');
+        Serial.print('K');
         break;
       case 'L':
-        Serial.write('L');
+        Serial.print('L');
         break;
       case 'M':
-        Serial.write('M');
+        Serial.print('M');
         break;
       case 'N':
-        Serial.write('N');
+        Serial.print('N');
         break;
       case 'O':
-        Serial.write('O');
+        Serial.print('O');
         break;
       case 'P':
-        Serial.write('P');
+        Serial.print('P');
         break;
       case 'Q':
-        Serial.write('Q');
+        Serial.print('Q');
         break;
       case 'R':
-        Serial.write('R');
+        Serial.print('R');
         break;
       case 'S':
-        Serial.write('S');
+        Serial.print('S');
         break;
       case 'T':
-        Serial.write('T');
+        Serial.print('T');
         break;
       case 'U':
-        Serial.write('U');
+        Serial.print('U');
         break;
       case 'V':
-        Serial.write('V');
+        Serial.print('V');
         break;
       case 'W':
-        Serial.write('W');
+        Serial.print('W');
         break;
       case 'X':
-        Serial.write('X');
+        Serial.print('X');
         break;
       case 'Y':
-        Serial.write('Y');
+        Serial.print('Y');
         break;
       case 'Z':
-        Serial.write('Z');
+        Serial.print('Z');
         break;
       case 'a':
-        Serial.write('a');
+        Serial.print('a');
         break;
       case 'b':
-        Serial.write('b');
+        Serial.print('b');
         break;
       case 'c':
-        Serial.write('c');
+        Serial.print('c');
         break;
       case 'd':
-        Serial.write('d');
+        Serial.print('d');
         break;
       case 'e':
-        Serial.write('e');
+        Serial.print('e');
         break;
       case 'f':
-        Serial.write('f');
+        Serial.print('f');
         break;
       case 'g':
-        Serial.write('g');
+        Serial.print('g');
         break;
       case 'h':
-        Serial.write('h');
+        Serial.print('h');
         break;
       case 'i':
-        Serial.write('i');
+        Serial.print('i');
         break;
       case 'j':
-        Serial.write('j');
+        Serial.print('j');
         break;
       case 'k':
-        Serial.write('k');
+        Serial.print('k');
         break;
       case 'l':
-        Serial.write('l');
+        Serial.print('l');
         break;
       case 'm':
-        Serial.write('m');
+        Serial.print('m');
         break;
       case 'n':
-        Serial.write('n');
+        Serial.print('n');
         break;
       case 'o':
-        Serial.write('o');
+        Serial.print('o');
         break;
       case 'p':
-        Serial.write('p');
+        Serial.print('p');
         break;
       case 'q':
-        Serial.write('q');
+        Serial.print('q');
         break;
       case 'r':
-        Serial.write('r');
+        Serial.print('r');
         break;
       case 's':
-        Serial.write('s');
+        Serial.print('s');
         break;
       case 't':
-        Serial.write('t');
+        Serial.print('t');
         break;
       case 'u':
-        Serial.write('u');
+        Serial.print('u');
         break;
       case 'v':
-        Serial.write('v');
+        Serial.print('v');
         break;
       case 'w':
-        Serial.write('w');
+        Serial.print('w');
         break;
       case 'x':
-        Serial.write('x');
+        Serial.print('x');
         break;
       case 'y':
-        Serial.write('y');
+        Serial.print('y');
         break;
       case 'z':
-        Serial.write('z');
+        Serial.print('z');
         break;
       case '!':
-        Serial.write('!');
+        Serial.print('!');
         break;
       case '@':
-        Serial.write('@');
+        Serial.print('@');
         break;
       case '#':
-        Serial.write('#');
+        Serial.print('#');
         break;
       case '$':
-        Serial.write('$');
+        Serial.print('$');
         break;
       case '%':
-        Serial.write('%');
+        Serial.print('%');
         break;
       case '^':
-        Serial.write('^');
+        Serial.print('^');
         break;
       case '&':
-        Serial.write('&');
+        Serial.print('&');
         break;
       case '*':
-        Serial.write('*');
+        Serial.print('*');
         break;
     }
   }
